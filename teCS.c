9de1b642a5469db03568135696a340d56b5aef4f
kurtberkan <berkan.kurt@stud.unibas.ch>
2022-05-21 15:27:35 +0200

    add comment to main

--- teCS.c
+++ teCS.c
@@ -9,7 +9,12 @@ void rawMode() {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_input);
 
 }
-
+/**
+ * The keyboard input gets red into the variable c.
+ * The while loop reads 1 byte from the standart input into c. It keeps doing it
+ * until there are no more bytes to read().
+ * @return read() if there are bytes that it red. else 0 if it reaches the end of the file.
+ */
 int main() {
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
