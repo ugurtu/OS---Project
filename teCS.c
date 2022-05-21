05aef2db76963028902cbac916f931f6366163b3
ugurtu <ugur.turhal@me.com>
2022-05-21 15:24:07 +0200

    put in rawmode

--- teCS.c
+++ teCS.c
@@ -1,4 +1,15 @@
 #include <unistd.h>
+#include <termios.h>
+
+void rawMode() {
+
+    struct termios raw_input;
+    tcgetattr(STDIN_FILENO, &raw_input);
+    raw_input.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_input);
+
+}
+
 int main() {
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
