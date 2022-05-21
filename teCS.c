63e45ead0250d89176072df398289e2009708365
ugurtu <ugur.turhal@me.com>
2022-05-21 16:52:51 +0200

    step1 finished

--- teCS.c
+++ teCS.c
@@ -1,11 +1,21 @@
-#include <unistd.h>
-#include <termios.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
 struct termios orig_termios;
 
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void deactivateRawMode() {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    die("tcsetattr");
 }
 
 /**
@@ -16,13 +26,20 @@ void deactivateRawMode() {
  * all pending output to be written to the terminal, and also discards any input that hasn’t been read.
  */
 void activateRawMode() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
 
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(deactivateRawMode);
 
     struct termios raw_input = orig_termios;
-    raw_input.c_lflag &= ~(ECHO);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_input);
+    raw_input.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw_input.c_oflag &= ~(OPOST);
+    raw_input.c_cflag |= (CS8);
+    raw_input.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw_input.c_cc[VMIN] = 0;
+    raw_input.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_input) == -1) die("tcsetattr");
 
 }
 
@@ -33,8 +50,17 @@ void activateRawMode() {
  * @return read() if there are bytes that it red. else 0 if it reaches the end of the file.
  */
 int main() {
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-    return 0;
+    activateRawMode();
+
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
+    }
 }
 
